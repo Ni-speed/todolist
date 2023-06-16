c82ee8a392b03f86e2d3020fe5cfd26ca8fce4a9
Ni-speed <railmir@gmail.com>
2023-06-16 16:54:21 +0500

    React-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,8 @@ import {Header} from '../components/Header/Header';
 import {Container} from "@mui/material";
 import {TodoListsList} from "../features/TodolistsList/TodoListsList";
 import {ErrorSnackbar} from "../components/ErrorSnackbar/ErrorSnackbar";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {Login} from "../features/Login/Login";
 
 
 function App() {
@@ -13,7 +15,12 @@ function App() {
             <ErrorSnackbar/>
             <Header/>
             <Container fixed>
-                <TodoListsList/>
+                <Routes>
+                    <Route path={'/'} element={<TodoListsList />} />
+                    <Route path={'/login'} element={<Login/>} />
+                    <Route path={'/404'} element={<h1>404 Page note found </h1>} />
+                    <Route path={'*'} element={<Navigate to={'404'}/>} />
+                </Routes>
             </Container>
         </div>
     );
